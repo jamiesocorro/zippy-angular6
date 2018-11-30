7bb84fcfa0a0ccde2efedec9b275f2d84d049dac
Jamie Socorro <jamie_socorro@live.com>
2018-11-30 06:44:10 -0700

    modified css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'zippy-angular6';
+
+  recommendations = [];
+
+  constructor() {
+    this.recommendations = ['Hairdresser','Plumber','UX Designer','Frontend Developer','Digital Marketer'];
+  }
+
+  search($event) {
+    let q = $event.target.value;
+  }
 }
