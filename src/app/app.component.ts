9d7849011f001046b762d3735e3686132700540b
Jamie Socorro <jamie_socorro@live.com>
2018-12-04 03:48:55 -0700

    modified css and added functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,43 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   title = 'zippy-angular6';
+  isLocation: boolean;
+  isSearch: boolean;
+  position: string;
 
   recommendations = [];
 
   constructor() {
-    this.recommendations = ['Hairdresser','Plumber','UX Designer','Frontend Developer','Digital Marketer'];
+    this.recommendations = ['Hairdresser', 'Plumber', 'UX Designer', 'Frontend Developer', 'Digital Marketer'];
+    this.isLocation = false;
+    this.isSearch = false;
+  }
+
+  chooseRecommendation(recommendation){
+    this.position = recommendation;
+    this.showSearch();
+  }
+
+  disable() {
+    this.isLocation = !this.isLocation;
   }
 
   search($event) {
-    let q = $event.target.value;
+    let q = $event.key;
+
+    var recommendations = this.recommendations.filter(function (recommendation) {
+      return recommendation.includes(q);
+    });
+
+    this.recommendations = recommendations;
+
   }
+
+  showSearch() {
+    this.isSearch = !this.isSearch;
+  }
+
+
 }
